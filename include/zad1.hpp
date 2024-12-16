d1643d62c50f723dc9934a130087e1956ff33ab3
mark1382 <mark1382@wp.pl>
2024-12-16 14:02:07 +0100

    pliki kolos

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,8 +1,12 @@
 #include "catch.hpp"
+#include <iostream>
 
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
+
+std::cout << "Probna kompilacja sie powiodla" << std::endl;
+
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
